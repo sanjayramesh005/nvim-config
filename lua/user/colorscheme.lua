a8d257e90e577cc6d27c9d2a11d59cf3e6d9815e
sanjay <sanjayramesh005@gmail.com>
2023-04-16 14:01:06 +0530

    minor changes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,38 @@
 local colorscheme = "tokyonight"
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- tokyonight config
+local status_ok, config = pcall(require, "tokyonight")
+if status_ok then
+	config.setup({
+		transparent = true,
+		sidebars = { "qf", "vista_kind" },
+    floats = {"packer", "terminal"},
+		styles = {
+			sidebars = "transparent", -- style for sidebars, see below
+			floats = "dark", -- style for floating windows
+		},
+	})
+end
+
+-- catppuccin config
+status_ok, _ = pcall(require, "catppuccin")
+if status_ok then
+	local catppuccin_config
+	status_ok, catppuccin_config = pcall(require, "trouble.config")
+	if status_ok then
+		catppuccin_config.setup({
+			flavour = "macchiato",
+			background = {
+				-- :h background
+				light = "latte",
+				dark = "macchiato",
+			},
+			transparent_background = true,
+		})
+	end
+end
+
+status_ok, config = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
--- lua/user/trouble.lua
+++ lua/user/trouble.lua
@@ -1,12 +1,11 @@
-local status_ok, trouble = pcall(require, "trouble")
+local status_ok, _ = pcall(require, "trouble")
 if not status_ok then
 	return
 end
-local status_ok, config = pcall(require, "trouble.config")
-if not status_ok then
+local ok, config = pcall(require, "trouble.config")
+if not ok then
 	return
 end
 
 config.setup({
-
 })
